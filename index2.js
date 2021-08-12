3f31772276b9bb50712de0338c3676129033d12f
Dunice Practice <dunicepractice@Mac-mini-Dunice.local>
2021-08-12 18:03:52 +0300

    js

--- index2.js
+++ index2.js
@@ -11,13 +11,13 @@ function clickReaction () {
     for ( let y = 0; y<30; y++){
         for( let x = 0; x<10; x++){
             const elem = document.createElement("div");
-            elem.classList.add('cell');
+            elem.classList.add('cell'); 
             gamefield.appendChild(elem);
             const metadata = {
                 x: x,
                 y: y,
-                isPlayer: false,
-                isBlock: false,
+                isPlayer: false,  
+                isBlock: false,         
                 element: elem,
             };
             cells.push(metadata);
@@ -34,11 +34,16 @@ function clickReaction () {
 
 function tick() {
     for (const cell of cells) {
-        cell.element.classList.remove('is-player');
-        cell.element.classList.remove('is-block');
-    }
-    for (const cell of playerState.points) {
-        cell.element.classList.add('is-player');
+        if (cell.isPlayer) {
+            cell.element.classList.remove('is-block');
+            cell.element.classList.add('is-player');
+        } else if (cell.isBlock) {
+            cell.element.classList.remove('is-player');
+            cell.element.classList.add('is-block');
+        } else {
+            cell.element.classList.remove('is-player');
+            cell.element.classList.remove('is-block');
+        }
     }
     setTimeout(tick, 1000);
 }
@@ -48,3 +53,27 @@ const el = document.getElementById("submit");
 el.addEventListener("click",clickReaction,);
 
 
+function moveUp(event){
+    if (event.code == 'ArrowUp') {
+        const playerCells = cells.filter((cell) => cell.isPlayer);
+        for (const cell of playerCells) {
+            cell.isPlayer = false;
+        }
+
+        console.log('Current', playerCells);
+
+        const newCoords = playerCells
+            .map((cell) => {
+                return {x: cell.x, y: cell.y - 1}
+            });
+
+        console.log('new', newCoords);
+
+        for (const coord of newCoords) {
+            const cell = cells.find((cell) => coord.x === cell.x && coord.y === cell.y)
+            cell.isPlayer = true;
+        }
+
+    }};
+
+document.addEventListener('keydown',moveUp);
